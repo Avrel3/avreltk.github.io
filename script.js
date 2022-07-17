efca8e18986c6dc7b09da4e27ad1620252b3875d
Avrel <avinash3pkhrl@gmail.com>
2022-07-17 09:35:59 +0545

    !

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log('Oh! We have a programming interested visitor in the console');
+console.log('Oh! We have a Programming Interested Visitor in the Console')
+console.log(new Date());
+console.log(navigator.userAgent);
